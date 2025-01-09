21f8f4aeb7323fbf3f7698dd0ae43b0655f76448
suryansh <livebeauty9@gmail.com>
2025-01-09 07:41:17 +0530

    This is change happend

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,51 @@
-import Image from "next/image";
+import {BrainCogIcon,EyeIcon,GlobeIcon,MonitorSmartphoneIcon, ServerCogIcon,ZapIcon} from "lucide-react"
+
+const feature = [
+  {
+    name: "Store your PDF Documents",
+    description: "Keep all your important PDF files securely stored and easily accessible anytime, anywhere",
+    icon: GlobeIcon
+  },
+  {
+    name: "Blazing Fast Responses",
+    description: "Experience lighting-fast answer to your queries, ensuring you get the information you need instantly",
+    icon: ZapIcon 
+  },
+  {
+    name: "Chat Memorisation",
+    description: "Our intelligent chatbot remembers previous interactions, providing a seamless and personalized experience",
+    icon:  BrainCogIcon
+  },
+  {
+    name: "Interactive PDF viewer",
+    description: "Enagge with your PDFs like never before using our intuitive and interactive viewer",
+    icon:  EyeIcon
+  },
+  {
+    name: "Cloud Backup",
+    description: "Rest assured knowing your documents are safely backend up on the cloud, protected from loss or damage",
+    icon:  ServerCogIcon
+  },
+  {
+    name: "Responsive Across Device",
+    description: "Access and chat with your PDFs seamlessly on any device, whether it's your desktop, tablet, or smartphone",
+    icon:  MonitorSmartphoneIcon
+  }
+]
+
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+   
+      <main className="flex-1 overflow-scroll p-2 lg:p-5 bg-gradient-to-bl from-white to-indigo-600">
+         <div className="bg-white py-24 sm:py-32 rounded-md drop-shadow-xl">
+           <div className="flex flex-col justify-center items-center max-w-7xl mx-auto px-6 lg:px-8">
+             <div className="mx-auto max-w-2xl sm:text-center">
+              <h2 className="text-base font-semibold leading-7 text-indigo-600">Your Interactive Document Companion</h2>
+             </div>
+         </div>
+       </div> 
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
   );
 }
